81280918632a18fe9d9df05f9cd096030ce6e690
shran24-bit <236351533+shran24bit@users.noreply.github.com>
2026-07-20 13:06:14 +0100

    Revert "ESNZ test spec and tests related folders are added"
    This reverts commit 15d45387e6f0c6729bbf4666b7c7b1bbf0bfd38f.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,8 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             % "api-test-runner_3"  % "0.10.0",
-    "org.slf4j"               % "slf4j-api"          % "2.0.17",
-    "io.rest-assured"         % "rest-assured"       % "6.0.0",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.2",
-    "org.scalatest"          %% "scalatest"          % "3.2.19",
-    "uk.gov.hmrc"            %% "totp-generator"     % "1.0.0"
+    "uk.gov.hmrc"     % "api-test-runner_3" % "0.10.0",
+    "org.slf4j"       % "slf4j-api"         % "2.0.17",
+    "io.rest-assured" % "rest-assured"      % "6.0.0"
   ).map(_ % Test)
 }
